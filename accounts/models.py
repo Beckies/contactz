5e8713d9668f58b08d85b79cf47d35fac5da8b6e
Beckies <24975530+Beckies@users.noreply.github.com>
2017-11-18 23:26:57 +0100

    CustomUser to User

--- accounts/models.py
+++ accounts/models.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
 
 
 class OrgAdmin(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     organization = models.ForeignKey(Organization, related_name='org_admins')
 
